ed79b2e9b965f58c6d530247d062d25b405216bc
Saad Khan <saadkhan23jan@gmail.com>
2022-09-26 22:03:14 +0200

    initial commit

--- Algorithms.py
+++ Algorithms.py
@@ -2,4 +2,90 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report, plot_confusion_matrix
+from sklearn.tree import DecisionTreeClassifier
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+
+
+def heatmap_plot_confusion_matrix(cm, labels, title="Confusion Matix"):
+    data = go.Heatmap(z=cm, y=labels, x=labels)
+    annotations = []
+    for i, row in enumerate(cm):
+        for j, value in enumerate(row):
+            annotations.append(
+                {"x": labels[i],
+                 "y": labels[j],
+                 "font": {"color": "white"},
+                 "text": str(value),
+                 "xref": "x1",
+                 "yref": "y1",
+                 "showarrow": False
+                 }
+            )
+            layout = {
+                "title": title,
+                "xaxis": {"title": "Predicted value"},
+                "yaxis": {"title": "Real value"},
+                "annotations": annotations
+            }
+            fig = go.Figure(data=data, layout=layout)
+            return fig
+
+def ff_plot_confusion_matrix(z, x, y):
+    # change each element of z to type string for annotations
+    z_text = [[str(y) for y in x] for x in z]
+
+    # set up figure
+    fig = ff.create_annotated_heatmap(z, x=x, y=y, annotation_text=z_text, colorscale='Viridis')
+
+    # add title
+    fig.update_layout(title_text='<i><b>Confusion matrix</b></i>',
+                      # xaxis = dict(title='x'),
+                      # yaxis = dict(title='x')
+                      )
+
+    # add custom xaxis title
+    fig.add_annotation(dict(font=dict(color="black", size=14),
+                            x=0.5,
+                            y=-0.15,
+                            showarrow=False,
+                            text="Predicted value",
+                            xref="paper",
+                            yref="paper"))
+
+    # add custom yaxis title
+    fig.add_annotation(dict(font=dict(color="black", size=14),
+                            x=-0.35,
+                            y=0.5,
+                            showarrow=False,
+                            text="Real value",
+                            textangle=-90,
+                            xref="paper",
+                            yref="paper"))
+
+    # adjust margins to make room for yaxis title
+    fig.update_layout(margin=dict(t=50, l=200))
+
+    # add colorbar
+    fig['data'][0]['showscale'] = True
+
+    return fig
+
+
+def decision_tree(df):
+    X = pd.get_dummies(df.drop('species', axis=1), drop_first=True)
+    y = df['species']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
+    model = DecisionTreeClassifier()
+    model.fit(X_train, y_train)
+    base_pred = model.predict(X_test)
+
+    cm = confusion_matrix(y_test,base_pred)
+    labels = list(df['species'].unique())
+    fig = ff_plot_confusion_matrix(cm, labels, labels)
+    return fig
+
+
 
--- app.py
+++ app.py
@@ -1,13 +1,89 @@
 import dash
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, dash_table
 from dash.dependencies import Input, Output, State
+import pandas as pd
+from Algorithms import decision_tree
+
+df = pd.read_csv('penguins_size.csv')
+df = df.dropna()
+df = df[df['sex']!='.']
 
 app = Dash(__name__)
 app.layout = html.Div([
 
-    html.P("Hello World")
+    html.Div([
+        dcc.Dropdown(id='df_actions', options=[{'label': 'Information', 'value': 'Information'},
+                                            {'label': 'Is Null', 'value': 'Is Null'},
+                                            {'label': 'Drop NA', 'value': 'Drop NA'},
+                                            {'label': 'Head', 'value': 'Head'}]),
+        dash_table.DataTable(id='df_actions_output'),
+    ]),
+
+    html.Div([
+        html.Label('Show the DataFrame'),
+        dcc.RadioItems(id='show_df', options=[{'label': 'Yes', 'value': 'Yes'},
+                                              {'label': 'No', 'value': 'No'}],
+                       value='No'),
+    ]),
+
+    html.Div(id='df_div',
+        children = [
+            dash_table.DataTable(id='dataframe', style_table={'overflowX': 'auto'},
+                             style_cell={'height': 'auto', 'minWidth': '100px', 'width': '100px',
+                                         'maxWidth': '180px', 'whiteSpace': 'normal'},
+                             style_header={'backgroundColor': 'white', 'fontWeight': 'bold'},
+                             style_data={'color': 'black', 'backgroundColor': 'white', 'border': '2px solid blue'},
+                             filter_action='native'
+                             ),
+             ], hidden=True),
+    html.Br(),
+
+    dcc.RadioItems(id='show_cm', options=[{'label': 'Yes', 'value': 'Yes'},
+                                          {'label': 'No', 'value': 'No'}],
+                   value='No'),
+
+    html.Div(id='show_cm_graph',
+        children = [
+            dcc.Graph(id='confusion_matrix'),
+        ], hidden=True),
+
+
 ])
 
+
+@app.callback(Output('df_div', 'hidden'),
+              Input('show_df', 'value'),
+              prevent_initial_call=True)
+def df_div(show_df):
+    if show_df == "No":
+        return True
+    else:
+        return False
+
+
+@app.callback([Output('show_cm_graph', 'hidden'),
+               Output('confusion_matrix', 'figure'), ],
+              [Input('show_cm', 'value'), ],
+              prevent_initial_call=True)
+def cm_graph(show_cm):
+    if show_cm == "No":
+        return True
+    else:
+        fig = decision_tree(df)
+        return False, fig
+
+
+@app.callback([Output('dataframe', 'data'),
+               Output('dataframe', 'columns'), ],
+              [Input('show_df', 'value'), ], )
+def update_df(show_df):
+
+
+    columns = [{'name': col, 'id': col} for col in df.columns]
+    df_table = df.to_dict(orient='records')
+
+
+    return df_table, columns
 
 
 
